864e24a8b8359310b4c9bdfa91368737166653e8
Chen Frederick <frederickfd88@gmail.com>
2021-09-04 22:09:42 +0700

    Added Laravel Routes

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,43 @@ use Illuminate\Database\Connection;
 |
 */
 
+// -------
+// General
+// -------
+
 Route::get('/', function () {
+    return view('home');
+})->name('home');
+
+Route::get('/palette-community', function () {
+    return view('palette.index');
+})->name('palette-community');
+
+Route::get('/creator', function () {
+    return view('creator');
+})->name('creator');
+
+// -------
+// Auth
+// -------
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/logout', function () {
+    //
+})->name('logout');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+// -------
+// DB Connection Test Route + Laravel Welcome Blade Page
+// -------
+
+Route::get('/laravel', function () {
     // Test database connection
     try {
         DB::connection()->getPdo();
