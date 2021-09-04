2d44c142f846eac911d56cb8e4d2491a59f7a434
Chen Frederick <frederickfd88@gmail.com>
2021-09-04 16:34:23 +0700

    Setup SCSS Usage, Tailwind.css, Fontawesome & Google Fonts Configuration

---
+++ resources/views/palette/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+    <h1>hi bro</h1>
+    <button>hello yes</button>
+    <i class="fa fa-address-card" aria-hidden="true"></i>
+</div>
+@endsection
