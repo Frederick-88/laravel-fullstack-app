4e8bc95a715f3ce9ca28482afd086559ce612d9c
Chen Frederick <frederickfd88@gmail.com>
2021-09-10 20:42:22 +0700

    Done Navbar UI

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,22 +1,22 @@
 <nav class="navbar">
     <ul class="flex items-center mr-5">
-        <li>
+        <li class="font-semibold">
             <a href="{{ route('home') }}" class="p-3 hover:text-gray-300">Home</a>
         </li>
-        <li>
+        <li class="font-semibold">
             <a href="{{ route('palette-community') }}" class="p-3 hover:text-gray-300">Palette Community</a>
         </li>
     </ul>
 
     <ul class="flex items-center ml-5">
         @auth
-        <li>
-            <a href="" class="p-3">{{ auth()->user()->name }}</a>
+        <li class="font-semibold">
+            <p class="p-3 inline">{{ auth()->user()->name }}</p>
         </li>
         <li>
             <form action="{{ route('logout') }}" method="post" class="p-3 inline">
                 @csrf
-                <button type="submit">Logout</button>
+                <button type="submit" class="font-semibold hover:text-gray-300">Logout</button>
             </form>
         </li>
         @endauth
@@ -25,11 +25,11 @@
         {{-- it is a directive to laravel for authenticated users & unauthenticated --}}
 
         @guest
-        <li>
+        <li class="font-semibold">
             <a href="{{ route('login') }}" class="p-3 hover:text-gray-300">Login</a>
         </li>
-        <li>
-            <a href="{{ route('register') }}" class="p-3 hover:text-gray-300">Register</a>
+        <li class="font-semibold">
+            <a href="{{ route('register.index') }}" class="p-3 hover:text-gray-300">Register</a>
         </li>
         @endguest
     </ul>
