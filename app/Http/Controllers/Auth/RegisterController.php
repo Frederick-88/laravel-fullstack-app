26a28492a2d2c81dae6345825525bdbeb527e2f3
Chen Frederick <frederickfd88@gmail.com>
2021-09-11 16:45:08 +0700

    Done Authentication - Registration

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Hash;
+
+use Auth;
+use DB;
+use Log;
+
+// models
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
+    public function index(Request $request)
+    {
+        return view('auth.register');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // "confirmed" validation in password will automatically bind with request of "password_confirmation" that is found in the input name attribute
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+        ]);
+
+        DB::beginTransaction(); // use DB transaction if something breaks along the process
+
+        try {
+            User::create([
+                'name' => $request->name,
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+            DB::commit();
+
+            // way to login -> https://laravel.com/docs/7.x/authentication
+            Auth::attempt([
+                'email' => $request->email,
+                'password' => $request->password
+            ]);
+
+            return Redirect::route('home');
+        } catch (\Exception $error) {
+            DB::rollBack();
+            Log::error($error);
+            Redirect::back();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
