db13ec449914c8828b22ec41f2ee2cd19784d6f1
Chen Frederick <frederickfd88@gmail.com>
2021-09-11 16:44:54 +0700

    Done Authentication - Login

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        // filling the remember_token column if the remember me checkbox is checked.
+        // then in web-cookies when you inspect there will be a key like "remember_web_59ba36addc2b2f9401580f014c7f58ea4e30989d"
+        // then if want to create function can try, like bind with database, etc.
+        $isRemember = $request->remember;
+
+        if (!auth()->attempt($request->only('email', 'password'), $isRemember)) {
+            return back()->with('status', 'Invalid login details');
+        }
+
+        return redirect()->route('palette-community');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
